d763981e5f1e52851f5aff4160831632b9079a8a
Vivek <viveksingh.singh50@gmail.com>
2022-04-04 00:24:46 +0530

    added table

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import CovidTable from './components/CovidTable';
+
+function App() {
+  return (
+    <div className="App">
+      <CovidTable />
+    </div>
+  );
+}
+
+export default App;
